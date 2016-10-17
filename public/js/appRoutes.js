910b42a2716d31669a9d24b02f3338594e2d88bd
developer2 <savedmed93@gmail.com>
2016-10-17 14:02:09 +0300

    angular Route - OK

--- public/js/appRoutes.js
+++ public/js/appRoutes.js
@@ -1,4 +1,4 @@
-angular.module('appRoutes', ['ngRoute'])
+angular.module('bsApp', ['ngRoute'])
   .config(['$routeProvider', '$locationProvider',
   function($routeProvider, $locationProvider){
     $routeProvider
